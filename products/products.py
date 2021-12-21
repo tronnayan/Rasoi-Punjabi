4c5f9c27acd4f9e8c9101eeb8a4cb4015abb9574
Nayan Arora <nayanarora55@gmail.com>
2021-12-21 22:08:13 +0530

    get product API fixed
    Signed-off-by: Nayan Arora <nayanarora55@gmail.com>

--- products/products.py
+++ products/products.py
@@ -38,8 +38,9 @@ def get_product(req: Request, db: Session = Depends(get_db)):
     token = req.headers["Authorization"]
     if crud.verify_token(token,credentials_exception="404"):
         product_data = db.query(models.Product).all()
-        temp = product_data.__dict__
-        temp["images"]  = db.query(models.Image).filter(models.Image.product == product_data.id).all()
+        for p in product_data:
+            temp = product_data.__dict__
+            temp["images"]  = db.query(models.Image).filter(models.Image.product == p.id).all()
         return product_data
     else:
         return HTTPException(status_code=404,detail="User not registered")
